8e2c65ca7f550035bbdaa74b812eb7e59a83c709
Woolfixti <63717269+Woolfixti@users.noreply.github.com>
2024-01-29 18:53:13 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Obserwuje serwer","Obserwuje serwer","Obserwuje serwer"];
 
 
 let currentIndex = 0;
